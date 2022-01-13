bbb4f9f81cc802d50594a525da760cd79ccdfe27
LAPTOP-HP <strugglingforbetter@gmail.com>
2022-01-13 09:22:25 +0800

    1st commit

---
+++ CudaLearning/CheckDimension/check_dimension.cu
@@ -0,0 +1,31 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include <device_launch_parameters.h>
+
+__global__ void checkIndex(void) {
+	printf("threadIdx: (%d, %d, %d) blockIdx: (%d, %d, %d) blockDim: (%d, %d, %d)\
+	gridDim(%d, %d, %d)\n", threadIdx.x, threadIdx.y, threadIdx.z, blockIdx.x, blockIdx.y,
+		blockIdx.z, blockDim.x, blockDim.y, blockDim.z, gridDim.x, gridDim.y, gridDim.z);
+}
+
+__global__ void hello_world(void) {
+	printf("GPU: Hello world! \n");
+}
+
+int main(int argc, char** argv) {
+
+
+	printf("CPU: Hello world! \n");
+	hello_world << <1, 10 >> > ();
+	cudaDeviceReset();  // if no this line, it can not output hello world from gpu
+
+	int nElem = 6;
+	dim3 block(3);
+	dim3 grid((nElem + block.x - 1) / block.x);
+	printf("grid.x %d grid.y %d grid.z %d \n", grid.x, grid.y, grid.z);
+	printf("block.x %d block.y %d block.z %d \n", block.x, block.y, block.z);
+
+	checkIndex << <grid, block >> > ();
+	cudaDeviceReset();
+	return 0;
+}
